c5450626f93bb8b4f17f2cee65824555c388e3c7
FabSyntaxDev <fabio.dsm.eng.software@gmail.com>
2026-04-26 21:54:38 -0300

    feat: implement database connection and user insertion using pgx

--- main.go
+++ main.go
@@ -11,38 +11,30 @@ import (
 )
 
 func main() {
-	// 1. Carrega o arquivo .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Erro ao carregar o arquivo .env")
 	}
 
-	// 2. Busca as variáveis do ambiente
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASSWORD")
 	name := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 
-	// 3. Monta a string de conexão
-	// Usamos 'localhost' porque o Go está no Windows e o Banco no Docker
 	connStr := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s", user, pass, port, name)
 
-	// 4. Estabelece a conexão com o banco
 	ctx := context.Background()
 	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
 		log.Fatalf("Erro ao conectar ao banco: %v", err)
 	}
-	defer conn.Close(ctx) // Garante que a conexão feche ao terminar o programa
+	defer conn.Close(ctx)
 
 	fmt.Println("Conexão estabelecida com sucesso!")
 
-	// 5. Dados para inserir (Simulando uma entrada de usuário)
-	novoNome := "FabSyntaxDev"
+	novoNome := "test"
 	novoEmail := "contato@fabsyntax.dev"
 
-	// 6. Executa o comando INSERT
-	// O $1 e $2 são substituídos pelos valores de novoNome e novoEmail com segurança
 	sql := "INSERT INTO usuarios (nome, email) VALUES ($1, $2)"
 
 	_, err = conn.Exec(ctx, sql, novoNome, novoEmail)
